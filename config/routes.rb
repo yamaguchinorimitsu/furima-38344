30e0f99bb0960b08e3e36acb7a641ad7ffc6c278
yamaguchinorimitsu <yamaguchi.nori1001@gmail.com>
2022-09-04 23:58:39 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :index] do
+  resources :items, only: [:new, :create, :index, :show] do
   end
 
 end
