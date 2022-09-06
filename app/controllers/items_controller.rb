78f1e1a29f1a09997bff6851fe34f4938da5941b
yamaguchinorimitsu <yamaguchi.nori1001@gmail.com>
2022-09-06 14:30:02 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,8 +15,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
