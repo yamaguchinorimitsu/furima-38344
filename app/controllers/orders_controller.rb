fc765be73fac26c6a7a41844f82a447ff7c1383b
yamaguchinorimitsu <yamaguchi.nori1001@gmail.com>
2022-09-08 18:24:18 +0900

    手順４　
    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,27 @@ class OrdersController < ApplicationController
 
 
   def index
-    @order = Order.new
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+  def order_params
+    params.require(:order).permit(:post_code, :prefecture_id, :city, :address, :building, :tell_number) .merge(user_id: current_user.id)
   end
 
   
 
-  
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture, :city, :house_number, :building, :tell_number, :order, :user_id, :item
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building, :tell_number, :order, :user_id
 
   with_options presence: true do
+    
+
     validates :city
     validates :house_number
     validates :user_id
@@ -10,10 +12,11 @@ class OrderAddress
     validates :tell_number, format:{with: /\A0[0-9]{9,10}\z/}
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
+
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create(item: :item, user_id: user_id)
+    order = Order.create(user_id: user_id)
     Address.create(tell_number: tell_number, postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building: building )
   end
 
