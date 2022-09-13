a9bdd28cb747aae2ecd1059172a1544fdcabb1cb
yamaguchinorimitsu <yamaguchi.nori1001@gmail.com>
2022-09-13 16:30:45 +0900

    item モデルのバリデーション変化等

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :order
 
-  validates :image, presence: true
+  validates :image, presence:{ message: 'を選択してください' }
   validates :item_name, presence: true
   validates :price, presence: true
   validates :content, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "が空なので登録できません" }
+  validates :condition_id, numericality: { other_than: 1, message: "が空なので登録できません" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "が空なので登録できません"}
+  validates :delivery_time_id, numericality: { other_than: 1, message: "が空なので登録できません" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "が空なので登録できません" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+                    format: { with: /\A[0-9]+\z/ } , allow_blank: true
 end
